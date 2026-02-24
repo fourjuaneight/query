cc1d1985774460918b677bbb1e3fa7a8a8f616bc
Juan Villela <fourjuaneight@users.noreply.github.com>
2026-02-24 17:10:41 -0500

    feat: Add local server for testing.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
 import { serve } from '@hono/node-server';
 
 import app from './app';
+import type { Bindings } from './common/typings';
 
 const port = Number(process.env['PORT'] ?? 8787);
 
+const env: Bindings = {
+  TMDB_KEY: process.env['TMDB_KEY'] ?? '',
+  IGDB_CLIENT_ID: process.env['IGDB_CLIENT_ID'] ?? '',
+  IGDB_ACCESS_TOKEN: process.env['IGDB_ACCESS_TOKEN'] ?? '',
+  DISCOGS_TOKEN: process.env['DISCOGS_TOKEN'] ?? '',
+  GITHUB_TOKEN: process.env['GITHUB_TOKEN'] ?? '',
+};
+
 console.log(`Server running at http://localhost:${port}`);
 
-serve({ fetch: app.fetch, port });
+serve({
+  fetch: request => app.fetch(request, env),
+  port,
+});
